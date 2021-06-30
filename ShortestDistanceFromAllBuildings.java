4de02c37af951482ca943835c1a117beeb268a48
Koustubh Mokashi <koustubh.mokashi@gmail.com>
2021-06-30 14:44:42 +0530

    Create ShortestDistanceFromAllBuildings.java

---
+++ ShortestDistanceFromAllBuildings.java
@@ -0,0 +1,91 @@
+class Solution {
+    
+    
+  private int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
+
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[][] grid = {{1,0,1},{0,0,0},{1,0,1}};
+        System.out.println(solution.shortestDistance(grid));
+    }
+
+    public int shortestDistance(int[][] grid) {
+        if (grid == null || grid.length == 0) return -1;
+        int rows = grid.length, cols = grid[0].length;
+        int[][] canReach = new int[rows][cols];
+        int[][] dist = new int[rows][cols];
+
+        int totalBuildings = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 1) {
+                    totalBuildings++;
+                    if (!traverse(grid, i, j, dist, canReach)) return -1;
+                }
+            }
+        }
+
+        int minDis = Integer.MAX_VALUE;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (canReach[i][j] == totalBuildings &&
+                        dist[i][j] < minDis) {
+                    minDis = dist[i][j];
+                }
+            }
+        }
+
+        return minDis == Integer.MAX_VALUE ? -1 : minDis;
+    }
+
+    private boolean traverse(int[][] grid, int row, int col, int[][] dist, int[][] canReach) {
+        int rows = grid.length, cols = grid[0].length;
+        boolean[][] visited = new boolean[rows][cols];
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{row,col});
+        visited[row][col] = true;
+        
+        int distance = 0;
+        
+        while(!q.isEmpty()) {
+            int size = q.size();
+            distance++;
+            for(int i=0;i<size;i++) {
+                int[] cur = q.poll();
+                int r = cur[0];
+                int c = cur[1];
+                
+                for(int k=0;k<4;k++) {
+                    int rr = directions[k][0]+r;
+                    int cc = directions[k][1]+c;
+                    if(!isValid(grid,rr,cc,visited)) continue;
+                    if(grid[rr][cc] == 0) {
+                        dist[rr][cc]+=distance;
+                        canReach[rr][cc]++;
+                        q.offer(new int[]{rr,cc});
+                    }
+                    visited[rr][cc] = true;
+                }
+            }
+        }
+        
+        for(int i=0;i<rows;i++) {
+            for(int j=0;j<cols;j++) {
+                if(!visited[i][j] && grid[i][j] == 1) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean isValid(int[][] grid, int rr, int cc, boolean[][] visited) {
+        if (rr > grid.length - 1 ||
+                rr < 0 || cc < 0 || cc > grid[0].length - 1) return false;
+        if (visited[rr][cc]) return false;
+        if(grid[rr][cc] == 2) return false;
+
+        return true;
+    }
+}
